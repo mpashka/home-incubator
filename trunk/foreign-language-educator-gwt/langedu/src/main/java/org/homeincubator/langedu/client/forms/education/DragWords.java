652927cb8c48d3f27ddb820a9a58416eb2792102
pavel.moukhataev@gmail.com <pavel.moukhataev@gmail.com@db8f4b9f-69d1-15ff-0465-16936968ba27>
2013-01-18 15:44:52 +0000

    dnd

--- trunk/foreign-language-educator-gwt/langedu/src/main/java/org/homeincubator/langedu/client/forms/education/DragWords.java
+++ trunk/foreign-language-educator-gwt/langedu/src/main/java/org/homeincubator/langedu/client/forms/education/DragWords.java
@@ -1,17 +1,44 @@
 package org.homeincubator.langedu.client.forms.education;
 
+import java.util.List;
+import org.homeincubator.langedu.client.EducationPage;
+import org.homeincubator.langedu.client.Educator;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Node;
+import com.google.gwt.dom.client.TableElement;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 
 /**
  */
-public class DragWords {
+public abstract class DragWords implements EducationPage {
     interface OverlapWordsUiBinder extends UiBinder<DivElement, DragWords> {}
     private static OverlapWordsUiBinder ourUiBinder = GWT.create(OverlapWordsUiBinder.class);
 
+    @UiField TableElement table;
+//    private
+
     public DragWords() {
         DivElement rootElement = ourUiBinder.createAndBindUi(this);
+
+    }
+
+    @Override
+    public void educate(List<Educator.WordEducation> words) {
+        Element child;
+        while ((child = table.getFirstChildElement()) != null) {
+            table.removeChild(child);
+        }
+
+        StringBuilder content = new StringBuilder();
+        for (Educator.WordEducation word : words) {
+
+        }
+    }
+
+    private static final class DraggableWordInfo {
 
     }
 }
