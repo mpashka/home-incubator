761dfb7b286a44d7b611ba0affb31aea09e8e02e
Pavel Moukhataev <m_pashka@mail.ru>
2022-02-11 20:58:32 +0300

    .

--- totemFTC/build.sh
+++ totemFTC/build.sh
@@ -6,22 +6,18 @@ root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # For testing
 #rm -rf $root/target
 target="$root/target/web"
-tmp="$root/target/tmp"
-mkdir -p $target
-mkdir -p $tmp
-
-#export DEMO_BUILD=true
+mkdir -p "$target"
 
 # RUN_PROFILE = {totem|demo}
 echo "Build profile $RUN_PROFILE"
 
 # Flutter
-curl "https://filebin.net/$file_bin/flutter-${RUN_PROFILE}.tar.gz" | tar -xz -C "$target"
+curl -L "https://filebin.net/$file_bin/flutter-${RUN_PROFILE}.tar.gz" | tar -xz -C "$target"
 
 # Quasar
-curl "https://filebin.net/$file_bin/quasar-${RUN_PROFILE}.tar.gz" | tar -xz -C "$target"
+curl -L "https://filebin.net/$file_bin/quasar-${RUN_PROFILE}.tar.gz" | tar -xz -C "$target"
 
 # Android apk
-curl "https://filebin.net/$file_bin/android-${RUN_PROFILE}.apk" -L -o "$target/anroid.apk"
+curl -L "https://filebin.net/$file_bin/android-${RUN_PROFILE}.apk" -o "$target/android.apk"
 
 cp -R "$root/docs/www/." "$target/"
